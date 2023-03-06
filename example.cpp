fea28b13f62b9c8a35b01dba3a720537c4a1f986
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-03-06 15:57:18 +0700

    First commit

---
+++ example.cpp
@@ -0,0 +1,23 @@
+//library
+#include <iostream>
+using namespace std;
+
+//progmam main
+int main () {//begin
+
+//numeric p,l,luas
+double p,l,luas
+//display "masukkan panjang =";
+cout <<"masukkan panjang =";
+//accept p
+cin>>p;
+//display "masukkan lebar =" ;
+cout<<"masukkan lebar =";
+//accept L 
+cin>>l;
+//compute luas=p*l
+luas=p*l;
+//display "luas persegi panjang adalah",luas
+cout<<"luas persegi panjang adalah"<<luas;
+
+}//end
